4f76e689b321fd16d584635e657fd778ea4a5dee
sserbin <sserbin@users.noreply.github.com>
2019-07-29 21:44:35 +0300

    stub environment improvements:
    - do not stub twig's builtin filters/tests/functions - builtins should now emit proper errors when used improperly;
    - fixed `noop` not working with nullary functions (it was expecting at
    least an unary one);

--- src/StubEnvironment.php
+++ src/StubEnvironment.php
@@ -15,6 +15,18 @@ class StubEnvironment extends Environment
      */
     public function getFilter($name)
     {
+        /**
+         * @var string[]
+         * @psalm-suppress InternalMethod
+         */
+        $defaultFilters = array_keys(parent::getFilters());
+        $isDefault = isset($defaultFilters[$name]) || $this->listContainsSubstring($defaultFilters, $name);
+
+        if ($isDefault) { // don't attempt to stub twig's builtin filter
+            /** @psalm-suppress InternalMethod */
+            return parent::getFilter($name);
+        }
+
         return new TwigFilter((string)$name, $this->noop(), [
             'is_variadic' => true,
         ]);
@@ -25,6 +37,18 @@ class StubEnvironment extends Environment
      */
     public function getFunction($name)
     {
+        /**
+         * @var string[]
+         * @psalm-suppress InternalMethod
+         */
+        $defaultFunctions = array_keys(parent::getFunctions());
+        $isDefault = isset($defaultFunctions[$name]) || $this->listContainsSubstring($defaultFunctions, $name);
+
+        if ($isDefault) { // don't attempt to stub twig's builtin function
+            /** @psalm-suppress InternalMethod */
+            return parent::getFunction($name);
+        }
+
         return new TwigFunction((string)$name, $this->noop(), [
             'is_variadic' => true,
         ]);
@@ -35,6 +59,18 @@ class StubEnvironment extends Environment
      */
     public function getTest($name)
     {
+        /**
+         * @var string[]
+         * @psalm-suppress InternalMethod
+         */
+        $defaultTests = array_keys(parent::getTests());
+        $isDefault = isset($defaultTests[$name]) || $this->listContainsSubstring($defaultTests, $name);
+
+        if ($isDefault) { // don't attempt to stub twig's builtin test
+            /** @psalm-suppress InternalMethod */
+            return parent::getTest($name);
+        }
+
         return new TwigTest((string)$name, $this->noop(), [
             'is_variadic' => true,
         ]);
@@ -45,7 +81,20 @@ class StubEnvironment extends Environment
         /**
          * @param mixed $_
          */
-        return function ($_, array $arg = []): void {
+        return function ($_ = null, array $arg = []): void {
         };
+    }
+
+    /**
+     * @param string[] $list
+     */
+    private function listContainsSubstring(array $list, string $needle): bool
+    {
+        foreach ($list as $item) {
+            if (false !== strpos($item, $needle)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
